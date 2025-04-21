31306c4a01f0dafe04c5c659819f194c3af1cdd7
matthew <likewise52@gmail.com>
2025-04-21 14:39:12 +0200

    Test apigatway lambda

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,39 @@ terraform {
     }
 }
 
+
+resource "aws_apigatewayv2_api" "test_api_gateway" {
+    name        = "games-api-gateway"
+    description = "API Gateway for games microservices"
+    protocol_type = "HTTP"
+
+    tags = {
+        Name = "games-api-gateway"
+    }
+}
+
+resource "aws_lambda_function" "test_lambda" {
+    function_name = "games-lambda-function"
+    handler = "index.handler"
+    runtime = "python3.11"
+    role = aws_iam_role.lambda_execution_role.arn
+    
+    tags = {
+        Name = "games-lambda-function"
+    }
+  
+}
+resource "aws_apigatewayv2_integration" "test_integration" {
+    api_id = aws_apigatewayv2_api.test_api_gateway.id
+    integration_type = "AWS_PROXY"
+    integration_uri = aws_lambda_function.test_lambda.invoke_arn
+    payload_format_version = "2.0"
+
+    depends_on = [aws_lambda_function.test_lambda]
+}
+
+resource "aws_apigatewayv2_route" "test_route" {
+    api_id = aws_apigatewayv2_api.api_gateway.id
+    route_key = "GET /games"
+    target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
